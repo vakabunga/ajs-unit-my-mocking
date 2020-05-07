55a80f8f308ee87d574bf7f9932a0cf35866efda
Vakabunga <shesterov@gmail.com>
2020-05-07 22:02:46 +0300

    do not split up getLevel to several functions

--- src/js/__tests__/fetchData.test.js
+++ src/js/__tests__/fetchData.test.js
@@ -1,28 +1,25 @@
 import fetchData from '../http';
-import { getLevel, showLevel } from '../app';
+import getLevel from '../app';
 
 jest.mock('../http');
 beforeEach(() => {
   jest.resetAllMocks();
 });
-test('getLevel: should call http with userid', () => {
-  fetchData.mockReturnValue('{}');
+describe('getLevel', () => {
+  test('getLevel: should call http with userid', () => {
+    fetchData.mockReturnValue('{}');
 
-  getLevel(1);
-  expect(fetchData).toBeCalledWith('https://server/user/1');
-});
-// test('fetchData: should show Error', () => {
-//   // fetchData.mockReturnValue('{}');
-//   function showError() {
-//     fetchData('https://server/user/1');
-//   }
-//   expect(showError).toThrowError(Error);
-// });
-test('should show user level', () => {
-  const userLvl = showLevel({ level: 10, status: 'ok' });
-  expect(userLvl).toBe('Ваш текущий уровень: 10');
-});
-test('should show error message', () => {
-  const userLvl = showLevel({ level: '', status: 'error' });
-  expect(userLvl).toBe('Информация об уровне временно недоступна');
+    getLevel(1);
+    expect(fetchData).toBeCalledWith('https://server/user/1');
+  });
+  test('should show user level', () => {
+    fetchData.mockReturnValue({ level: 10, status: 'ok' });
+    const response = getLevel(1);
+    expect(response).toBe('Ваш текущий уровень: 10');
+  });
+  test('should show error message', () => {
+    fetchData.mockReturnValue({ level: '', status: 'error' });
+    const response = getLevel(2);
+    expect(response).toBe('Информация об уровне временно недоступна');
+  });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,9 @@
 // TODO: write your code here
 import fetchData from './http';
 
-export function getLevel(userId) {
-  const response = fetchData(`https://server/user/${userId}`);
+export default function getLevel(userId) {
+  const data = fetchData(`https://server/user/${userId}`);
 
-  // TODO: логика обработки
-
-  return JSON.parse(response);
-}
-
-export function showLevel(data) {
   if (data.status === 'ok') {
     return `Ваш текущий уровень: ${data.level}`;
   }
